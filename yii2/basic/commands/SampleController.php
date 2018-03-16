c9b275f9e1dbeb6c848a26c3ef3d4cb18f84e7d7
Yasufumi Moritake <moritake@mediba.jp>
2018-03-16 18:33:34 +0900

    👍Add Yii2.

--- yii2/basic/commands/SampleController.php
+++ yii2/basic/commands/SampleController.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace app\commands;
 
@@ -13,21 +8,9 @@ use yii\console\ExitCode;
 use Throwable;
 use Exception;
 
-/**
- * This command echoes the first argument that you have entered.
- *
- * This command is provided as an example for you to learn how to create console commands.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class SampleController extends Controller
 {
-    /**
-     * This command echoes what you have entered as the message.
-     * @param string $message the message to be echoed.
-     * @return int Exit code
-     */
+
     public function actionIndex()
     {
         try {
