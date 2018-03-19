2ff48fec7787dd4bce965004ad0b4f5d89f8cf34
Yasufumi Moritake <moritake@mediba.jp>
2018-03-19 16:50:05 +0900

    👍Update error.

--- errors/error2.php
+++ errors/error2.php
@@ -4,11 +4,11 @@
 try {
     throw new Error('test');
 } catch (Exception $e) {
-    // PHP 7.1 で実行される。PHP 5.6 では実行エラーとなる。
+    // PHP 7.1 では実行されない。PHP 5.6 では実行エラーとなる。
     echo 'catch Exception!', PHP_EOL;
     var_dump($e);
 } catch (Throwable $t) {
-    // PHP 7.1/5.6 では到達しない。
+    // PHP 7.1 で実行される。PHP 5.6 では実行エラーとなる。
     echo 'catch Throwable!', PHP_EOL;
     var_dump($t);
 }
