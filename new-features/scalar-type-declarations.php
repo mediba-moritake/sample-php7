0b9ca60a683c0f7dd132394dc46da97f82dfa20b
Yasufumi Moritake <moritake@mediba.jp>
2018-03-20 10:52:53 +0900

    👍Update comment.

--- new-features/scalar-type-declarations.php
+++ new-features/scalar-type-declarations.php
@@ -11,6 +11,19 @@ function sumOfInts(int ...$ints)
 
 var_dump(sumOfInts(2, '3', 4.1));
 
+//Fatal error: Uncaught TypeError: Argument 2 passed to sumOfInts() must be of the type integer, string given, called in /sample-php7/new-features/scalar-type-declarations.php on line 12 and defined in /sample-php7/new-features/scalar-type-declarations.php:6
+//Stack trace:
+//#0 /sample-php7/new-features/scalar-type-declarations.php(12): sumOfInts(2, '3', 4.1)
+//#1 {main}
+//
+//Next TypeError: Argument 3 passed to sumOfInts() must be of the type integer, float given, called in /sample-php7/new-features/scalar-type-declarations.php on line 12 and defined in /sample-php7/new-features/scalar-type-declarations.php:6
+//Stack trace:
+//#0 /sample-php7/new-features/scalar-type-declarations.php(12): sumOfInts(2, '3', 4.1)
+//#1 {main}
+//  thrown in /sample-php7/new-features/scalar-type-declarations.php on line 6
+
+
+
 //array(3) {
 //  [0]=>
 //  int(2)
